a9e58e8022534f67dd430ae3d39c56bc8f1d5213
malakhusseinabdelhalem-arch <malak.husseinabdelhalem@gmail.com>
2025-11-12 15:15:45 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,33 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
-#include <string>
+#include <iostream>
+#include <String>
+
 using namespace std;
 
-//define animal class here 
-
-
-
+class Animal {
+  private:
+    String name;
+    int age;
+    boolean isHungry;
+  public:
+    Animal (String name ,int age, boolean isHungry){
+      this->name = name;
+      this->age = age;
+      this->isHungry =isHungry;
+    }
+  void display() {
+    cout<< "Animal's name = "<< name <<"his age:"<<age<<"The animal is :"<<isHungry<<endl; 
+  }
+  void feed(){
+    if (isHungry == true){
+      cout << "Tested ! the Animal is hungary"<<endl;
+    }else{
+      cout << "Tested ! the Animal is not hungary"<<endl;
+    }
+    
+  }
+};
 #endif
+
