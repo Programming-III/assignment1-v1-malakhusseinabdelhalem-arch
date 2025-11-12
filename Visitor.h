77565478f4a56081698ecfe908a06322fc5a8edd
malakhusseinabdelhalem-arch <malak.husseinabdelhalem@gmail.com>
2025-11-12 15:19:35 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,22 @@
 #ifndef VISITOR_H
 #define VISITOR_H
 
-#include <string>
+#include <iostream>
+#include <String>
 using namespace std;
 
-//define visitor class here
-
-
-
-
-
-
-
+class Visitor {
+  private:
+    String visitorName;
+    int ticketBought;
+  public:
+    Visitor (String visitorName,int ticketBought){
+      this->visitorName = visitorName;
+      this->ticketBought=ticketBought;
+      
+    }
+    void displayInfo(){
+      
+    }
+};
 #endif
