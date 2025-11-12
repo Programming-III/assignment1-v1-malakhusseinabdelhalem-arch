e9f2f692638d0459ee3a3dc8f904ab0bfc7f8c3f
malakhusseinabdelhalem-arch <malak.husseinabdelhalem@gmail.com>
2025-11-12 15:17:09 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,30 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
 
+#include <iostream>
+#include <String>
 #include "Animal.h"
 
-//define enclosure class here 
+class Enclosure:public Animal{
+  private :
+    Animal*animal;
+    int capacity;
+    int currentCount;
+  public:
+  Enclousre(Animal*animal ,int capacity, int currentCount){
+    this->animal=animal;
+    this->capacity=capacity;
+    this->currentCount=currentCount;
+    
+  }
+  void addAnimal(Animal * a){
+    
+  }
+  void displayAnimals(){
+    
+  }
+};
 
+  
 
-
-
-
-
-
-
-#endif
+#endif 
